3144ce87d7f3bc6145ffa298f0cd6e809f4a9a73
Jon-Whitaker <jonathan.whitaker@outlook.com>
2022-06-22 20:15:06 +0100

    Added the new files and ready to impprt modules

--- Python notes.py
+++ Python notes.py
@@ -932,9 +932,7 @@ for position in range(len(chosen_word)):
 ##-------------------------------------------------------------
 ##-------------------------------------------------------------
 
-# My Finished product:
-
-
+# My Finished Hangman product:
 
 
 import random
@@ -1006,7 +1004,102 @@ stages = ['''
 =========
 ''']
 lives = 7
-word_list = ["aardvark", "baboon", "camel"]
+word_list = [
+"aardvark", 
+"baboon", 
+"camel",
+"analytical",
+"assess",
+"conceptual",
+"constitutional",
+"creative",
+"distribution",
+"environmental",
+"illegal",
+"analyse",
+"analysis",
+"analyst",
+"analytic",
+"analyze",
+"approach",
+"area",
+"assessment",
+"assume",
+"assumed",
+"assuming",
+"assumption",
+"authoritative",
+"authority",
+"availability",
+"available",
+"beneficial",
+"beneficiary",
+"benefit",
+"concept",
+"conception",
+"consist",
+"consistency",
+"consistent",
+"consistently",
+"constituency",
+"constituent",
+"constitute",
+"constitution",
+"constitutive",
+"context",
+"contextual",
+"contextualize",
+"contract",
+"contractor",
+"create",
+"creation",
+"creator",
+"data",
+"define",
+"definition",
+"derivation",
+"derivative",
+"derive",
+"dissimilar",
+"distribute",
+"distributive",
+"distributor",
+"economic",
+"economical",
+"economically",
+"economics",
+"economist",
+"economy",
+"environment",
+"environmentalist",
+"establish",
+"established",
+"establishment",
+"estimate",
+"estimation",
+"evidence", 
+"evident",
+"evidential",
+"evidently",
+"export",
+"exporter",
+"factor",
+"finance",
+"financial",
+"financially",
+"formula",
+"formulate",
+"formulation",
+"function",
+"functional",
+"functionally",
+"identifiable",
+"identification",
+"identify",
+"identity",
+"illegality",
+"income",
+"inconsistency"]
 end_of_game = False
 display = []
 guessed_letters = []
@@ -1015,12 +1108,7 @@ chosen_word = random.choice(word_list)
 for i in chosen_word:
   display += "_"
 
-if "_" not in display:
-  end_of_game = True
-  print(f"\nCongratulations, you've Found the word is was '{dj}'\n")
-
-
-
+# print(chosen_word)
 
 while end_of_game != True:
   try:
@@ -1032,32 +1120,31 @@ while end_of_game != True:
       lives -= 1
       if lives == 0:
         end_of_game == True
-        print(f"Unfortunately '{guess}' isn't in there....You lost!\n {stages[lives]}")
+        print(f"Unfortunately '{guess}' isn't in there....You lost!\n\nThe word was {chosen_word}\n{stages[lives]}")
         break
       print(f"sorry, that guess is incorrect\n{stages[lives]}")
-    if guess in chosen_word:
+
+    elif guess in chosen_word:
       print(f"Excellent guess! '{guess}' is in there!\n")
     for position in range(word_len):
       letter = chosen_word[position]
       if letter == guess:
         display[position] = letter
+        if "_" not in display:
+          end_of_game = True
+          print(f"\nCongratulations, you've Found the word is was '{chosen_word}'\n")
+          break
+
     dj = "".join(display)
-    print(f"These are the letters you've tried so far:\n{guessed_letters}\nYou have '{lives}' lives left, guess again..\n\n{dj}")
+    if end_of_game == False:
+      print(f"These are the letters you've tried so far:\n{guessed_letters}\nYou have '{lives}' lives left, guess again..\n\n{dj}")
   except ValueError:
     print("Type in a letter")
 
 
 #tried a letter already?
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
 
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
-
-
-#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
 
 
 
